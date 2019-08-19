02008bcdcacfcdab4854973cdeb53a8b8494df92
GHOUFA <zohayeramine@gmail.com>
2019-08-19 16:39:55 +0200

    fosuser install

---
+++ src/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: GHOUFA Zouhayer Amine
+ * Date: 19/08/19
+ * Time: 15:11
+ */
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
